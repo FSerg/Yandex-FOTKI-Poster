22ce2e6520f360adc21ac2f54733dcdbbfe88f45
Sergey Filkin <fs@alternativa-pro.ru>
2016-01-02 21:44:22 +0300

    Added mongodb autorization support

--- backend/config.js
+++ backend/config.js
@@ -3,6 +3,9 @@
 // =================================
 var MONGODB_ADDR = process.env.MONGODB_PORT_27017_TCP_ADDR || 'localhost';
 var MONGODB_PORT = process.env.MONGODB_PORT_27017_TCP_PORT || 27017;
+var MONGODB_USER = process.env.MONGODB_ENV_MONGODB_USER || 'admin';
+var MONGODB_PASS = process.env.MONGODB_ENV_MONGODB_PASS || '123456';
+
 
 module.exports = {
 
@@ -10,7 +13,9 @@ module.exports = {
   // "database": "mongodb://<dbuser>:<dbpassword>@xxx.mongolab.com:xxxxx/dbname",
   // "database": process.env.MONGODB || "localhost",
   // MongoDB connection options
-  database: 'mongodb://' + MONGODB_ADDR + ':' + MONGODB_PORT + '/yandex-fotki-poster',
+  // database: 'mongodb://' + MONGODB_ADDR + ':' + MONGODB_PORT + '/yandex-fotki-poster',
+  // adding authorization :)
+  "database": 'mongodb://'+ MONGODB_USER +':'+ MONGODB_PASS +'@' + MONGODB_ADDR + ':' + MONGODB_PORT + '/yandex-fotki-poster',
 
   "port": process.env.PORT || 3000, // usually 3000, but for Tutum service need 80
   "secretKey": process.env.SECRETKEY || "My super secret key"
