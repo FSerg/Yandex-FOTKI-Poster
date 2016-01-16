7dd2e036e2f2be5a112cca252935ce43657077bf
Sergey Filkin <fs@alternativa-pro.ru>
2016-01-16 19:46:10 +0300

    Changed connections options to MongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,23 +1,25 @@
 
 // Production specific configuration
 // =================================
-var MONGODB_ADDR = process.env.MONGODB_PORT_27017_TCP_ADDR || 'localhost';
-var MONGODB_PORT = process.env.MONGODB_PORT_27017_TCP_PORT || 27017;
-var MONGODB_USER = process.env.MONGODB_ENV_MONGODB_USER || 'admin';
-var MONGODB_PASS = process.env.MONGODB_ENV_MONGODB_PASS || '123456';
+var MONGODB_ADDR = process.env.MONGODB_ENV_TUTUM_IP_ADDRESS || process.env.MONGODB_ADDRESS || 'localhost';
+var MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+var MONGODB_USER = process.env.MONGODB_USER || 'admin';
+var MONGODB_PASS = process.env.MONGODB_ENV_MONGODB_PASS || process.env.MONGODB_PASS || '123456';
 
+var MONGO_CONNECTION;
+if (process.env.NODE_ENV === 'production') {
+    MONGO_CONNECTION = 'mongodb://'+ MONGODB_USER +':'+ MONGODB_PASS +'@' + MONGODB_ADDR + ':' + MONGODB_PORT + '/yandex-fotki-poster';
+}
+else {
+    MONGO_CONNECTION = 'mongodb://' + MONGODB_ADDR + ':' + MONGODB_PORT + '/yandex-fotki-poster';
+}
 
 module.exports = {
+    // example database config string
+    // "database": "mongodb://<dbuser>:<dbpassword>@xxx.mongolab.com:xxxxx/dbname",
+    "database": MONGO_CONNECTION,
 
-  // example database config string
-  // "database": "mongodb://<dbuser>:<dbpassword>@xxx.mongolab.com:xxxxx/dbname",
-  // "database": process.env.MONGODB || "localhost",
-  // MongoDB connection options
-  // database: 'mongodb://' + MONGODB_ADDR + ':' + MONGODB_PORT + '/yandex-fotki-poster',
-  // adding authorization :)
-  "database": 'mongodb://'+ MONGODB_USER +':'+ MONGODB_PASS +'@' + MONGODB_ADDR + ':' + MONGODB_PORT + '/yandex-fotki-poster',
-
-  "port": process.env.PORT || 3000, // usually 3000, but for Tutum service need 80
-  "secretKey": process.env.SECRETKEY || "My super secret key"
+    "port": process.env.PORT || 3000, // usually 3000, but for Tutum service need 80
+    "secretKey": process.env.SECRETKEY || "My super secret key"
 
 };
